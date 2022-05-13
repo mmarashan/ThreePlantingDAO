35fedb6d669980668a49ddee98f9a0c7c4d6a849
mrmx <mrmx@MacBook-Pro-mrmx.local>
2022-05-14 00:38:16 +0400

    Add new methods to contract

--- contracts/TreePlantingDAO.sol
+++ contracts/TreePlantingDAO.sol
@@ -8,6 +8,24 @@ pragma solidity ^0.8.0;
 contract ThreePlantingDAO {
 
     /**
+     Issue created event
+    */
+    event IssueCreated(string externalId);
+
+    /**
+     Issue started and fund sent event
+    */
+    event IssueStarted(
+        string externalId, 
+        address indexed executor
+    );
+
+    /**
+     Issue executed event
+    */
+    event IssueExecuted(string externalId);
+
+    /**
      Minimim confirmations count for send funds
      */
     uint public requiredConfirmationsCount;
@@ -16,22 +34,22 @@ contract ThreePlantingDAO {
      DAO's owners
      */
     address[] public owners;
-    mapping(address => bool) public isOwner;
 
     /**
     Issues created by DAO's owners
     @param externalId external identifier of issue
     @param executor executor's address
-    @param value funds for issue executing
+    @param amount funds for issue executing
     @param description description of issue
     @param isExecuted if the issue exicuted
     @param numConfirmations count of confirmations
     */
     struct ThreePlantingIssue {
         string externalId;
-        uint value;
+        uint amount;
         bytes description;
         address executor;
+        bool isFundsSent;
         bool isExecuted;
         uint numConfirmations;
     }
@@ -45,10 +63,21 @@ contract ThreePlantingDAO {
      Modifier checks only one of owners can executes funds sending to executor
      */
     modifier onlyOwner() {
-        require(isOwner[msg.sender], "Not owner");
+        bool isOwner = false;
+        for (uint i = 0; i < owners.length; i++) {
+            if (msg.sender == owners[i]) {
+                isOwner = true;
+            }
+        }
+        require(isOwner, "Not owner");
         _;
     }
 
+    /**
+     Contract's contructor
+     @param _owners list of DAO's owners
+     @param _requiredConfirmationsCount minimim confirmations count for send funds
+     */
     constructor(address[] memory _owners, uint _requiredConfirmationsCount) {
         require(_owners.length > 0, "Owners required");
         require(
@@ -58,11 +87,7 @@ contract ThreePlantingDAO {
 
         for (uint i = 0; i < _owners.length; i++) {
             address owner = _owners[i];
-
             require(owner != address(0), "Invalid owner");
-            require(!isOwner[owner], "Owner not unique");
-
-            isOwner[owner] = true;
             owners.push(owner);
         }
 
@@ -70,9 +95,89 @@ contract ThreePlantingDAO {
     }
 
     /**
-    @return Contract's balance
+     @return Contract's balance
      */
     function balance() public view returns (uint) {
         return address(this).balance;
+    }
+
+    /**
+     Create new issue
+     */
+    function createIssue(
+        string memory externalId,
+        uint value,
+        bytes memory description
+    ) public onlyOwner {
+        require(balance() > value, "Funds sum should't more than ccontract's balance");
+
+        ThreePlantingIssue memory issue = ThreePlantingIssue(
+            externalId,
+            value,
+            description,
+            address(0),
+            false,
+            false,
+            0
+        );
+        issues[externalId] = issue;
+        emit IssueCreated(externalId);
+    }
+
+    /**
+     Sets issue executor
+     @param executor executor's addresss
+     */
+    function setIssueExecutor(
+        string memory externalId,
+        address executor
+    ) public onlyOwner {
+        ThreePlantingIssue storage issue = issues[externalId];
+        require(issue.executor == address(0), "Issue already has executor");
+        issue.executor = executor;
+
+        emit IssueStarted(externalId, executor);
+    }
+
+    /**
+     Confirm issue
+     */
+    function confirmIssue(
+        string memory externalId
+    ) public onlyOwner {
+        ThreePlantingIssue storage issue = issues[externalId];
+        require(issue.executor != address(0), "Issue has't executor");
+        issue.numConfirmations += 1;
+
+        sendFundsIfHasRequiredConfirmation(issue);
+    }
+
+    /**
+     Check count of confirmations and send funds to executor
+     */
+    function sendFundsIfHasRequiredConfirmation(
+        ThreePlantingIssue storage _issue
+    ) private onlyOwner {
+        if (_issue.numConfirmations == requiredConfirmationsCount) {
+            (bool success, ) = _issue.executor.call{value: _issue.amount}(_issue.description);
+            require(success, "Failed to send Ether");
+
+            _issue.isFundsSent = true;
+            emit IssueStarted(_issue.externalId, _issue.executor);
+        }
+    }
+
+    /**
+     Mark issue as executed
+     Simplifications: one owner can set issue as executed
+     */
+    function markAsExecuted(
+        string memory externalId
+    ) public onlyOwner {
+        ThreePlantingIssue storage issue = issues[externalId];
+        require(issue.isFundsSent, "Issue not started");
+
+        issue.isExecuted = true;
+        emit IssueExecuted(externalId);
     }
 }
